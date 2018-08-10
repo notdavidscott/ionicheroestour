4108a44929182a9def3efb5117f02668aedc5950
David Scott Perez <david.scott.perez@live.com>
2018-08-09 21:27:05 -0700

    heroes tour iOS and Android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,13 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//NgModel
+import { FormsModule } from '@angular/forms'; 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp), 
+    FormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
